d20850bc34e5363e571e380a04ef7f8e763d62f6
Rodrigo Costa <rodrigocosta168@hotmail.com>
2015-10-29 14:17:19 -0200

    ok

---
+++ app/src/main/java/com/example/rodrigocosta/myapplication/User.java
@@ -0,0 +1,16 @@
+package com.example.rodrigocosta.myapplication;
+
+/**
+ * Created by rodrigocosta on 29/10/15.
+ */
+public class User {
+    private String _name;
+
+    public String getName() {
+        return _name;
+    }
+
+    public void setName(String value) {
+        _name = value;
+    }
+}
---
+++ app/src/test/java/com/example/rodrigocosta/myapplication/fallingTest.java
@@ -0,0 +1,16 @@
+package com.example.rodrigocosta.myapplication;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by rodrigocosta on 29/10/15.
+ */
+public class fallingTest {
+
+    @Test
+    public void failingTest() {
+       // Assert.assertFalse(true);
+    }
+
+}
